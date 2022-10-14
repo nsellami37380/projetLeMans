fc3bad42db82a88fc13d8f33f88ea129289091b5
Gwendal41 <gwendal41@hotmail.fr>
2022-10-14 12:00:36 +0200

    home page and routing header

--- leMans24/src/app/app-routing.module.ts
+++ leMans24/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageContainerListComponent } from './pages/page-container-list/page-container-list.component';
 import { PageHomeComponent } from './pages/page-home/page-home.component';
 
 const routes: Routes = [
-  {path:'', component: PageHomeComponent }
+  {path:'', component: PageHomeComponent },
+  {path:'home', component: PageHomeComponent },
+  {path:'pilots', component: PageContainerListComponent},
+  {path:'teams', component: PageContainerListComponent},
+  {path:'cars', component: PageContainerListComponent},
 
 ];
 
