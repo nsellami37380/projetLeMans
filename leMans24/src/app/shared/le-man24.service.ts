dd302c4287e77c0ad6c05407e78e4ca5796e5de0
Gwendal41 <gwendal41@hotmail.fr>
2022-10-18 15:17:34 +0200

    Same yesterday

--- leMans24/src/app/shared/le-man24.service.ts
+++ leMans24/src/app/shared/le-man24.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import { Pilot } from '../models/pilot.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +9,7 @@ export class LeMan24Service {
 
   constructor(private http: HttpClient) { }
 
-  getPilots(): Observable<Pilot[]> {
-    return this.http.get<Pilot[]>("assets/leMans24-db.json");
+  getPilots(): Observable<any[]> {
+    return this.http.get<any[]>("assets/leMans24-db.json");
   }
 }
