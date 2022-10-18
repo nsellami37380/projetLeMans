c4eb025c3c4ee0d79ac6e8c5b329f8a2b20837c7
Noureddine <nourredinesellami@hotmail.com>
2022-10-18 15:14:10 +0200

    code for GB Json

--- leMans24/src/app/components/container-list/container-list.component.ts
+++ leMans24/src/app/components/container-list/container-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Car } from 'src/app/models/car.model';
 import { Pilot } from 'src/app/models/pilot.model';
+import { Team } from 'src/app/models/team.model';
 import { LeMan24Service } from 'src/app/shared/le-man24.service';
 
 @Component({
@@ -10,7 +12,15 @@ import { LeMan24Service } from 'src/app/shared/le-man24.service';
 })
 export class ContainerListComponent implements OnInit {
 
-  objectList: any = [];
+  //objectList: any = [];
+
+  // pilotList: Pilot[] = [];
+  // teamList: Team[] = [];
+  // carList: Car[] = [];
+
+  ptcList: Pilot[] | Team[] | Car[] = [];
+  title: string = "";
+
   url: string = '';
 
   constructor(private route: ActivatedRoute,
@@ -19,13 +29,28 @@ export class ContainerListComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe((param: ParamMap)=>{
       this.url = param.get('var') as string;
-    })
+      this.getList();
+    })    }
 
-    this.leMan24S.getPilots().subscribe(PilotsFromJsonFile => {
-      this.objectList = PilotsFromJsonFile;
-      console.log(this.objectList);
-    });
+
+    
+    getList(): void{
+      if (this.url ==='pilots'){
+        // this.leMan24S.getPilots().subscribe(pilots  => this.pilotList = pilots)
+         this.leMan24S.getPilots().subscribe(pilots  => this.ptcList = pilots)
+        this.title = "Listes des pilotes";
+      } else
+      if (this.url ==='cars'){
+         this.leMan24S.getCars().subscribe(cars  => this.ptcList = cars)
+        this.title = "Listes des voitures";
+        // this.leMan24S.getCars().subscribe(cars  => this.carList = cars)
+      } else
+      if (this.url ==='teams'){
+        // this.leMan24S.getTeams().subscribe(teams  => this.teamList = teams)
+        this.title = "Listes des écuries";
+         this.leMan24S.getTeams().subscribe(teams  => this.ptcList = teams)
+      };
+
 
   }
-
 }
