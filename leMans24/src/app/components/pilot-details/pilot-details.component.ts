d0965fbf5b15a785afd29110f22742cbe90c0d36
Gwendal41 <gwendal41@hotmail.fr>
2022-10-20 13:46:08 +0200

    delete page detail and add routing card

--- leMans24/src/app/components/pilot-details/pilot-details.component.ts
+++ leMans24/src/app/components/pilot-details/pilot-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Pilot } from 'src/app/models/pilot.model';
+import { Team } from 'src/app/models/team.model';
+import { LeMan24Service } from 'src/app/shared/le-man24.service';
 
 @Component({
   selector: 'app-pilot-details',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pilot-details.component.scss']
 })
 export class PilotDetailsComponent implements OnInit {
+  
+  id:number = 0;
+  pilotDetail: Pilot = new Pilot (0,[],'','',(new Date),0,(new Team(0,'','',[],[],0,'')),'','');
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private leMan24S: LeMan24Service) { }
+
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((param: ParamMap)=>{
+    if(param.get('id')){
+      this.id = parseInt(param.get("id") as string);
+    }
+  }) 
   }
-
 }
