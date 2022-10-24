0c654bbb968e1c1b858cb5496fc3308a8fc209dd
Gwendal41 <gwendal41@hotmail.fr>
2022-10-24 17:02:12 +0200

    refont css

--- leMans24/src/app/app.module.ts
+++ leMans24/src/app/app.module.ts
@@ -44,7 +44,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
