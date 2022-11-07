29366bd4c63e8a0a1310c96c500e9fca2686a8c7
nsellami37380 <82585130+nsellami37380@users.noreply.github.com>
2022-11-07 11:27:54 +0100

    Add files via upload

---
+++ leMans24Back/Project/src/main/java/com/lemans24/Project/LeMans24Application.java
@@ -0,0 +1,13 @@
+package com.lemans24.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeMans24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeMans24Application.class, args);
+	}
+
+}
---
+++ leMans24Back/Project/src/test/java/com/lemans24/Project/LeMans24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemans24.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeMans24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
