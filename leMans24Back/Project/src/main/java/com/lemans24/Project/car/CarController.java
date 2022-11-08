fe28ef284d4c4d6d172c807986910e3923bda476
Noureddine <nourredinesellami@hotmail.com>
2022-11-08 16:28:28 +0100

    Post from angular

--- leMans24Back/Project/src/main/java/com/lemans24/Project/car/CarController.java
+++ leMans24Back/Project/src/main/java/com/lemans24/Project/car/CarController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/cars")
 
--- leMans24Back/Project/src/main/java/com/lemans24/Project/carphoto/CarPhotoController.java
+++ leMans24Back/Project/src/main/java/com/lemans24/Project/carphoto/CarPhotoController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/cars-photos")
 public class CarPhotoController {
