251423994d52c995ee5e12dd4a7f789b6c2fb9e8
Marvin <bourhanemarvin@hotmail.fr>
2022-11-24 17:03:52 +0100

    toast added in home if not logged in

--- leMans24/src/app/components/home/home.component.ts
+++ leMans24/src/app/components/home/home.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { timer } from 'rxjs';
+import { AuthService } from 'src/app/shared/auth.service';
 import { LeMan24Service } from 'src/app/shared/le-man24.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  providers: [MessageService]
+  providers: [MessageService],
 })
 export class HomeComponent implements OnInit {
-
   constructor(private messageService: MessageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    (async () => {
+      await this.setDelay(5000);
 
+      this.connectPlease();
+    })();
   }
+
+  setDelay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  connectPlease() {
+    this.messageService.clear();
+    this.messageService.add({
+      key:"c",
+      sticky: true,
+      severity: 'warn',
+      summary: 'Are you sure?',
+      detail: 'Confirm to proceed',
+    });
+  }
+}
