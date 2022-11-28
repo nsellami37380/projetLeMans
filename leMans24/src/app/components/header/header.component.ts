371732b44bb87cc033359a9c47dd09df0dea0270
Pilou <pilou@mbp-de-pilou.home>
2022-11-28 22:52:20 +0100

    rebuilt home page, added scroll tracking and fixed some incorrect synthaxes

--- leMans24/src/app/components/header/header.component.ts
+++ leMans24/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {MenuItem} from 'primeng/api';
+import { Component, Input, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { filter } from 'rxjs';
 import { ERole } from 'src/app/models/enum/ERole.enum';
 import { AuthService } from 'src/app/shared/auth.service';
 
@@ -12,58 +13,73 @@ import { AuthService } from 'src/app/shared/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input() percentageScrolled: number = 0;
+  @Input() threshold: number = 0;
+
+  isHome: boolean = false;
+
   menu: MenuItem[] = [];
   visibleSidebar1: boolean = false;
   isAdmin: boolean = false;
   isUser: boolean = false;
   isManager: boolean = false;
-  username:string = "";
+  username: string = "";
   isConnected: boolean = false;
-  role:string ="";
+  role: string = "";
 
-  constructor(private router: Router,
-             private authS: AuthService) {}
+  constructor(
+    private router: Router,
+    private authS: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.router.events.forEach((event) => {
+      if(event instanceof NavigationStart) {
+        if(event.url === "/" || event.url === "/home") {
+          this.isHome = true;
+        } else {
+          this.isHome = false;
+        }
+      }
+    });
+
     this.isAdmin = this.authS.getAppUser().roleList.includes(ERole.ROLE_ADMIN)
-    console.log(this.isAdmin);
-  
 
     this.authS.appUser$.subscribe(appuser => {
       this.isAdmin = appuser.roleList.includes(ERole.ROLE_ADMIN)
       this.isUser = appuser.roleList.includes(ERole.ROLE_USER)
       this.isManager = appuser.roleList.includes(ERole.ROLE_MANAGER)
-      this.isConnected = appuser.username !='';
+      this.isConnected = appuser.username != '';
       this.username = appuser.username;
-      if(this.isConnected && appuser.roleList.includes(ERole.ROLE_ADMIN)){
+      if (this.isConnected && appuser.roleList.includes(ERole.ROLE_ADMIN)) {
         this.role = 'Administrateur';
-      }else if(this.isConnected && appuser.roleList.includes(ERole.ROLE_MANAGER)){
+      } else if (this.isConnected && appuser.roleList.includes(ERole.ROLE_MANAGER)) {
         this.role = "Manager";
-      }else if(this.isConnected && appuser.roleList.includes(ERole.ROLE_USER)){
+      } else if (this.isConnected && appuser.roleList.includes(ERole.ROLE_USER)) {
         this.role = "";
       }
     })
-  
 
-    this.menu=[
+
+    this.menu = [
       {
         label: 'Ajouter',
-        items:[{
-                label:'Une écurie', "routerLink": '/addTeam'                
-              },
-              {
-                label:'Une voiture', "routerLink": '/addCar'
-              },
-              {
-                label:'Un pilote', "routerLink": '/addPilot'
-              }],
-       }
+        items: [{
+          label: 'Une écurie', "routerLink": '/addTeam'
+        },
+        {
+          label: 'Une voiture', "routerLink": '/addCar'
+        },
+        {
+          label: 'Un pilote', "routerLink": '/addPilot'
+        }],
+      }
     ]
 
- 
+
   }
 
-  logOut(){
+  logOut() {
     this.authS.logOut();
   }
 }
---
+++ leMans24/src/app/shared/scroll-util.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScrollUtilService {
+
+  percentageScrolled$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  threshold: number = 10;
+
+  constructor() { }
+
+  getCurrentScroll() {
+    const winheight= window.innerHeight || (document.documentElement || document.body).clientHeight
+    const docheight = this.getDocHeight()
+    const scrollTop = window.pageYOffset;
+    const trackLength = docheight - winheight
+    const pctScrolled = Math.floor(scrollTop/trackLength * 100)
+    this.percentageScrolled$.next(pctScrolled);    
+  }
+
+  getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    )
+  }
+
+}
